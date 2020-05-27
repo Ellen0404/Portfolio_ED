a4bc0658aa707d665aaea144b14e59ea179cdf11
Elena Demidionok <elenademidionok@Elenas-MacBook-Pro.local>
2020-05-27 14:50:04 -0600

    react deployment

---
+++ build/precache-manifest.c6c36a37aec019460dbb8008313ce2fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be867c0d94a26c6cfecdfe19f2c9701",
+    "url": "/Portfolio_ED/index.html"
+  },
+  {
+    "revision": "027451df25dab26ced79",
+    "url": "/Portfolio_ED/static/css/main.45603c24.chunk.css"
+  },
+  {
+    "revision": "e0fd0d23b6e24b2a5848",
+    "url": "/Portfolio_ED/static/js/2.46618f42.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio_ED/static/js/2.46618f42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "027451df25dab26ced79",
+    "url": "/Portfolio_ED/static/js/main.07b73de7.chunk.js"
+  },
+  {
+    "revision": "76333374fe3b14915221",
+    "url": "/Portfolio_ED/static/js/runtime-main.0c145bb6.js"
+  }
+]);
