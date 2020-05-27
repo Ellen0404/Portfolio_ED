afdeed566a782ea533892e35c945659f8399184f
Elena Demidionok <elenademidionok@Elenas-MacBook-Pro.local>
2020-05-27 15:17:19 -0600

    Updates

--- precache-manifest.c2d058d65e055a1bc5a6553c0c80daf8.js
+++ precache-manifest.c2d058d65e055a1bc5a6553c0c80daf8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2be867c0d94a26c6cfecdfe19f2c9701",
+    "revision": "9d89517c8f82130620c6af84ebe12230",
     "url": "/Portfolio_ED/index.html"
   },
   {
-    "revision": "027451df25dab26ced79",
+    "revision": "30e691a0c027d92ecaca",
     "url": "/Portfolio_ED/static/css/main.45603c24.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Portfolio_ED/static/js/2.46618f42.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "027451df25dab26ced79",
-    "url": "/Portfolio_ED/static/js/main.07b73de7.chunk.js"
+    "revision": "30e691a0c027d92ecaca",
+    "url": "/Portfolio_ED/static/js/main.538e8a62.chunk.js"
   },
   {
     "revision": "76333374fe3b14915221",
